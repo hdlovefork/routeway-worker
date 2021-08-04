5cdd2640d37e11b6c8c9190a30dd2d92f9c6efef
Dean <1602264241@qq.com>
2021-08-04 09:56:42 +0800

    修复可能导致多次请求表单验证的BUG

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -22,7 +22,7 @@ class ServiceProvider extends LaravelServiceProvider
     {
         //
     }
-    
+
     /**
      * Register the application services.
      *
@@ -37,28 +37,30 @@ class ServiceProvider extends LaravelServiceProvider
             return new Request();
         });
         $this->app->alias(Request::class, 'workerman.response');
-        
+
         $this->app->afterResolving(ValidatesWhenResolved::class, function ($resolved) {
-            Log::debug("\$resolved->client_id={$resolved->client_id}");
-            $resolved->validateResolved();
+            if($resolved instanceof SocketRequest){
+                Log::debug("\$resolved->client_id={$resolved->client_id}");
+                $resolved->validateResolved();
+            }
         });
-        
+
         $this->app->resolving(SocketRequest::class, function ($request, $app) {
             $request = SocketRequest::createFrom($app['workerman.request'], $request);
             $request->setContainer($app);
         });
-        
+
         $this->commands([
             WorkermanCommand::class,
             MakeHandlerCommand::class
         ]);
-        
+
         $this->publishes([
             __DIR__ . '/../routes/routeway.php' => base_path('routes/routeway.php'),
             __DIR__ . '/../config/routeway.php' => base_path('config/routeway.php'),
             __DIR__ . '/../resources/lang/en/routeway.php' => resource_path('lang/en/routeway.php'),
             __DIR__ . '/../resources/lang/zh-CN/routeway.php' => resource_path('lang/zh-CN/routeway.php'),
-            
+
         ], 'routeway');
     }
 }
